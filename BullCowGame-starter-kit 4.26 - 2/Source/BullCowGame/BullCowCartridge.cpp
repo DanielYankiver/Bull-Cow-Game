e5c2aa2aae97e2f246763b48343342d8f82d565e
DanielYankiver <69213153+DanielYankiver@users.noreply.github.com>
2021-07-05 14:11:23 -0400

    finished pseudocode

--- BullCowGame-starter-kit 4.26 - 2/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit 4.26 - 2/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,42 +1,51 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
-
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    // Welcoming the Player 
+    // Welcoming The Player
     PrintLine(TEXT("Welcome to Bull Cows!"));
     PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove!
     PrintLine(TEXT("Press enter to continue..."));
-
-    // Setting Up Game 
+    
+    // Setting Up Game
     HiddenWord = TEXT("cake"); // Set the HiddenWord
-    // Set Lives 
+    // Set Lives
+
+    // Prompt Player For Guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+    
+    // Checking PlayerGuess
 
     if (Input == HiddenWord)
     {
-      PrintLine(TEXT("You have Won!")); 
+        PrintLine(TEXT("You have Won!"));
     }
     else
     {
-       PrintLine(TEXT("You have Lost!")); 
+        PrintLine(TEXT("You have Lost!"));
 
-  
+        
     }
-    // Check if Isogram 
-    // Check if right number of characters 
+    // Check If Isogram
+    // Prompt To Guess Again
+    // Check Right Number Of Characters
+    // Prompt To Guess Again
 
-    // Remove Life 
+    // Remove Life
 
-    // Check if Lives > 0
-    // If Yes PlayAgain 
-    // If No Show GameOver
-    // Prompt to Play Again 
+    // Check If Lives > 0
+    // If Yes GuessAgain
+    // Show Lives Left
+    // If No Show GameOver and HiddenWord?
+    // Prompt To Play Again, Press Enter To Play Again?
+    // Check User Input
+    // PlayAgain Or Quit
+
 }
