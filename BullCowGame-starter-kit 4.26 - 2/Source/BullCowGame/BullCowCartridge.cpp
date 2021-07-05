b37115925ce025145d55f300c8b29c8934ccb055
DanielYankiver <69213153+DanielYankiver@users.noreply.github.com>
2021-07-05 12:54:42 -0400

    Added HiddenWord to header file and reconfigured the cartridge file

--- BullCowGame-starter-kit 4.26 - 2/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit 4.26 - 2/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,18 +1,20 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bull Cows!"));
     PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove!
     PrintLine(TEXT("Press enter to continue..."));
+    HiddenWord = TEXT("cake");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("cake"); // Move outside this function 
+
     if (Input == HiddenWord)
     {
       PrintLine(TEXT("You have Won!")); 
